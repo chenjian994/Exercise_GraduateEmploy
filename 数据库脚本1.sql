e484c618a07104b146d93f20ef70b04b3cd60911
chenjian <cj520025@gmail.com>
2013-08-10 14:46:44 +0800

    设计基本完成了，准备开始编码

--- 数据库脚本1.sql
+++ 数据库脚本1.sql
@@ -1,8 +1,40 @@
 /*==============================================================*/
 /* DBMS name:      Microsoft SQL Server 2008                    */
-/* Created on:     2013/8/7 11:32:27                            */
+/* Created on:     2013/8/10 14:42:56                           */
 /*==============================================================*/
 
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('t_graduate') and o.name = 'FK_T_GRADUA_REFERENCE_T_JOB')
+alter table t_graduate
+   drop constraint FK_T_GRADUA_REFERENCE_T_JOB
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('graduate_job')
+            and   name  = 'Relationship_3_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index graduate_job.Relationship_3_FK
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('graduate_job')
+            and   name  = 'Relationship_2_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index graduate_job.Relationship_2_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('graduate_job')
+            and   type = 'U')
+   drop table graduate_job
+go
 
 if exists (select 1
             from  sysobjects
@@ -39,6 +71,48 @@ if exists (select 1
            where  id = object_id('t_job')
             and   type = 'U')
    drop table t_job
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('tb_profile')
+            and   name  = 'Has_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index tb_profile.Has_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('tb_profile')
+            and   type = 'U')
+   drop table tb_profile
+go
+
+/*==============================================================*/
+/* Table: graduate_job                                          */
+/*==============================================================*/
+create table graduate_job (
+   id                   int                  not null,
+   no                   int                  not null,
+   constraint PK_GRADUATE_JOB primary key (id, no)
+)
+go
+
+/*==============================================================*/
+/* Index: Relationship_2_FK                                     */
+/*==============================================================*/
+create index Relationship_2_FK on graduate_job (
+id ASC
+)
+go
+
+/*==============================================================*/
+/* Index: Relationship_3_FK                                     */
+/*==============================================================*/
+create index Relationship_3_FK on graduate_job (
+no ASC
+)
 go
 
 /*==============================================================*/
@@ -78,6 +152,7 @@ go
 /*==============================================================*/
 create table t_graduate (
    no                   int                  identity,
+   id                   int                  null,
    name                 varchar(20)          null,
    username             varchar(20)          null,
    password             varchar(20)          null,
@@ -91,9 +166,7 @@ create table t_graduate (
    email                varchar(30)          null,
    address              varchar(50)          null,
    selfEval             varchar(255)         null,
-   certificate          varchar(255)         null,
    addition             varchar(255)         null,
-   score                varchar(255)         null,
    verified             bit                  null,
    constraint PK_T_GRADUATE primary key nonclustered (no)
 )
@@ -126,5 +199,28 @@ go
 create index enterprise_job_FK on t_job (
 t_e_id ASC
 )
+go
+
+/*==============================================================*/
+/* Table: tb_profile                                            */
+/*==============================================================*/
+create table tb_profile (
+   no                   int                  not null,
+   "desc"               text                 not null,
+   type                 varchar(100)         not null
+)
+go
+
+/*==============================================================*/
+/* Index: Has_FK                                                */
+/*==============================================================*/
+create index Has_FK on tb_profile (
+no ASC
+)
+go
+
+alter table t_graduate
+   add constraint FK_T_GRADUA_REFERENCE_T_JOB foreign key (id)
+      references t_job (id)
 go
 
